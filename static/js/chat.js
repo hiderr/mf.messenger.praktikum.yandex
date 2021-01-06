8a0cee67701e7e77b1da5f9712957b0478f966cd
Timofey Ponomarev <hiderrweb@yandex.ru>
2021-01-06 19:46:35 +0300

    added delete_chat action. changed classes for form__input

--- static/js/chat.js
+++ static/js/chat.js
@@ -5,4 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".paperclip").onclick = function () {
         document.querySelector(".messages__menu_attachments").classList.toggle("messages__menu_hidden");
     };
+
+    document.querySelector(".contacts__main").onclick = function (e) {
+        document.querySelector(".contact_selected").classList.remove("contact_selected");
+        e.target.closest("li").classList.add("contact_selected");
+    }
 });
