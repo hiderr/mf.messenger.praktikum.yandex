2d0abd437fd6ac75aa876779cce9353fae75e02a
Timofey Ponomarev <hiderrweb@yandex.ru>
2021-02-05 16:16:15 +0500

    Added HTTPTransport class

---
+++ static/modules/HTTPTransport.ts
@@ -0,0 +1,75 @@
+type Options = {
+    method?: string,
+    headers?: object,
+    data?: object,
+    timeout?: number
+};
+
+export class HTTPTransport {
+    METHODS = {
+        GET: 'GET',
+        PUT: 'PUT',
+        POST: 'POST',
+        DELETE: 'DELETE'
+    };
+
+    get = (url: string, options: Options = {}) => {
+        if (typeof options.data === "object") {
+            url = url + this.queryStringify(options.data);
+        }
+        return this.request(url, {...options, method: this.METHODS.GET}, options.timeout);
+    };
+
+    put = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: this.METHODS.PUT}, options.timeout);
+    };
+
+    post = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: this.METHODS.POST}, options.timeout);
+    };
+
+    delete = (url: string, options: Options = {}) => {
+        return this.request(url, {...options, method: this.METHODS.DELETE}, options.timeout);
+    };
+
+    queryStringify(data: object) {
+        return "?" + Object.keys(data).map(key => key + '=' + data[key]).join('&');
+    }
+
+    // PUT, POST, DELETE
+
+    // options:
+    // headers — obj
+    // data — obj
+    request = (url: string, options: Options, timeout: number = 5000) => {
+        const {method, headers, data} = options;
+
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url, true);
+
+            if (typeof headers === "object") {
+                Object.keys(headers).forEach(key => {
+                    xhr.setRequestHeader(key, headers[key]);
+                });
+            }
+
+            xhr.timeout = timeout;
+            xhr.withCredentials = true;
+
+            xhr.onload = function () {
+                resolve(xhr);
+            };
+
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+
+            if (method === this.METHODS.GET || !data) {
+                xhr.send();
+            } else {
+                xhr.send(JSON.stringify(data));
+            }
+        });
+    };
+}
